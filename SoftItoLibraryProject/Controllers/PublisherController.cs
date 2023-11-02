6aa93cb3b36e20b82d5555fa1d6fe2de532c7f1b
sinan <sinancivak00@gmail.com>
2023-11-02 03:27:45 +0300

    Yayınevleri

---
+++ SoftItoLibraryProject/Controllers/PublisherController.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoftItoLibraryProject.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PublisherController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetAll()
+        {
+            var values=_context.Publishers.ToList();
+            return View(values);
+        }
+    }
+}
